7d7ea323e7825812ff5c5c685ffebe58252cef31
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-09-30 21:10:29 -0400

    Added str_to_length and get_pretty_time to Functions.py.

--- General/Functions.py
+++ General/Functions.py
@@ -2,6 +2,13 @@ import os
 import json
 
 
+def str_to_length(base_str, length, do_dots=True):
+    ret_str = base_str.ljust(length)[:length]
+    if do_dots and len(base_str) + 3 >= length:
+        return ret_str[:-3] + "..."
+    return ret_str
+
+
 def get_curr_parent_dir():
     return os.path.dirname(os.getcwd()).replace("\\", "/")
 
@@ -11,3 +18,7 @@ def parse_json(json_path):
         raise FileExistsError
     with open(json_path) as f:
         return json.load(f)
+
+
+def get_pretty_time(some_datetime):
+    return some_datetime.strftime("%Y/%m/%d %I:%M%p")
