5b88d391fa3dee0c4bc693b7d912a692d6c1502d
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-10-02 20:46:35 -0400

    Renamed credentials_file_path in Constants.py to twitch_credentials_file_path

--- General/Constants.py
+++ General/Constants.py
@@ -1,4 +1,4 @@
 from General import Functions
 
 
-credentials_file_path = Functions.get_curr_parent_dir() + "/0 - Secrets/TwitchHelper/credentials.json"
+twitch_credentials_file_path = Functions.get_curr_parent_dir() + "/0 - Secrets/TwitchHelper/twitch_credentials.json"
